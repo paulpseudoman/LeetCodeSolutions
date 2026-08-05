bcfa725817997fe932d41270ce0e213372f1b79f
Aritrabha Majumdar <85665131+paulpseudoman@users.noreply.github.com>
2026-08-05 15:08:29 +0530

    Time: 27 ms (8.71%) | Memory: 41.5 MB (5.00%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows > 1){
+            string zigzag; 
+            for(int i = 0; i < numRows; i++){
+                int shift = 2 * (numRows - 1);
+                for (int j = i; j < s.size(); j = j + shift){
+                    zigzag = zigzag + s[j];
+                    if (i > 0 && i < numRows - 1 && j + shift < s.size() + 2 * i){
+                        zigzag = zigzag + s[j + shift - 2 * i];
+                    }
+                }
+
+            }
+            return zigzag;
+        }
+        else{
+            return s;
+        }
+        
+    }
+};
