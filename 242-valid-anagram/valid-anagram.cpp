87815e772c6c48e2fbe1a75853a1cc1dd15d573d
Aritrabha Majumdar <85665131+paulpseudoman@users.noreply.github.com>
2026-08-22 02:02:26 +0530

    Time: 1 ms (54.15%) | Memory: 9.6 MB (92.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
-            return false;
+        int count[26] = {};
+
+        for (char c : s)
+            count[c - 'a']++;
+
+        for (char c : t)
+            count[c - 'a']--;
+
+        for (int x : count) {
+            if (x != 0)
+                return false;
         }
-        else{
-        for(char c : t){
-            if(s.find(c) != string::npos){
-                s.erase(s.find(c), 1);
-            }
-        }
-        }
-        return s.empty();
+
+        return true;
     }
 };
